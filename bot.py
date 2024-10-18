743e8f59afc50ffd28910e17b4af35d8cffb2e97
Mehmet Ali Koylu <91757385+MehmetAliKOYLU@users.noreply.github.com>
2024-10-18 19:59:47 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 
 intents=discord.Intents(messages=True,guilds=True,reactions=True,members=True,presences=True,message_content = True)
 client=commands.Bot(command_prefix='-',intents=intents)
-token="MTAyNTg2MjUwNDkxMzU3MTk5MQ.GRcvPI.Wl46CNAVQi1BWZh7srY5MXxZFchIa3hK6js5pY"
+token=YourTokenAdress
 
 @client.event
 async def on_ready():
@@ -14,20 +14,7 @@ async def on_message(message):
     if message.author == client.user:
         return
     message.content=message.content.lower()
-    if message.content == 'qw':
-        await message.channel.send('en iyisi')
-    elif message.content=='sila':
-        await message.channel.send('SILAA')
-    elif message.content=='soner':
-        await message.channel.send('ZORBA')
-    elif message.content=='zort':
-        await message.channel.send('Zattiri zort zort')
-    elif message.content=='teto':
-        await message.channel.send('Valo gelcen mi')
-    elif message.content=='gorusuruz':
-        await message.channel.send('gorusuruz canim')
-    elif message.content=='bb':
-        await message.channel.send('gorusuruz canim')
-    elif message.content=='canim':
-        await message.channel.send('soyle balim')
+    if message.content == 'Hi':
+        await message.channel.send('Hello !')
+
 client.run(token)
